52ba50a5f27c5679a1c927c081ef2090d60e729e
Audrius <momantai0@gmail.com>
2013-07-30 18:06:21 +0300

    GetStringFromAudio2 (no need to save audiofile)

--- SpeechRecognizerService/src/java/org/me/Sphinx/Sphinx4Service.java
+++ SpeechRecognizerService/src/java/org/me/Sphinx/Sphinx4Service.java
@@ -13,6 +13,7 @@ import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
 import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
+import java.io.File;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
@@ -24,16 +25,16 @@ import java.net.URL;
 @WebService(serviceName = "Sphinx4Service")
 public class Sphinx4Service {
 
-    /**
-     * Web service operation
-     */
+    /**-----------------------------------------------------------------------
+     * Get string type text from Audio file (Audio file must be already saved)
+     -----------------------------------------------------------------------*/
     @WebMethod(operationName = "GetStringFromAudio")
     public String GetStringFromAudio() {
         String Text;
-        Transcriber aaa = new Transcriber();
+        Transcriber newMethod = new Transcriber();
         try
         {
-            Text = aaa.test();
+            Text = newMethod.Recognise();
         }
         catch(Exception e)
         {
@@ -44,11 +45,9 @@ public class Sphinx4Service {
     
     public class Transcriber {
         
-        public String test() throws IOException, UnsupportedAudioFileException {
+        public String Recognise() throws IOException, UnsupportedAudioFileException {
             URL audioURL;
-
-                audioURL = edu.cmu.sphinx.demo.transcriber.Transcriber.class.getResource("AudioFile.wav");
-
+            audioURL = edu.cmu.sphinx.demo.transcriber.Transcriber.class.getResource("AudioFile.wav");
             URL configURL = edu.cmu.sphinx.demo.transcriber.Transcriber.class.getResource("config.xml");
             ConfigurationManager cm = new ConfigurationManager(configURL);
             Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
@@ -57,6 +56,48 @@ public class Sphinx4Service {
             // configure the audio input for the recognizer
             AudioFileDataSource dataSource = (AudioFileDataSource) cm.lookup("audioFileDataSource");
             dataSource.setAudioFile(audioURL, null);
+            // Loop until last utterance in the audio file has been decoded, in which case the recognizer will return null.
+            Result result;
+            String stringline = "";
+            while ((result = recognizer.recognize())!= null) {
+                    stringline = stringline + ' ' + result.getBestResultNoFiller();
+                    System.out.println(stringline);
+            }
+            return stringline;
+        }
+    }
+
+    /**-----------------------------------------------------------------------
+     * Get string type text from Audio file (Audio file transmit through parameters)
+     -----------------------------------------------------------------------*/
+    @WebMethod(operationName = "GetStringFromAudio2")
+    public String GetStringFromAudio2(@WebParam(name = "AudioFile") File AudioFile) {
+        String Text;
+        Transcriber2 newMethod = new Transcriber2();
+        try
+        {
+            Text = newMethod.Recognise(AudioFile);
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+        return Text;
+    }
+    
+    public class Transcriber2 {
+        
+        public String Recognise(File AudioFile) throws IOException, UnsupportedAudioFileException {
+            URL audioURL;
+            audioURL = edu.cmu.sphinx.demo.transcriber.Transcriber.class.getResource("AudioFile.wav");
+            URL configURL = edu.cmu.sphinx.demo.transcriber.Transcriber.class.getResource("config.xml");
+            ConfigurationManager cm = new ConfigurationManager(configURL);
+            Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+            /* allocate the resource necessary for the recognizer */
+            recognizer.allocate();
+            // configure the audio input for the recognizer
+            AudioFileDataSource dataSource = (AudioFileDataSource) cm.lookup("audioFileDataSource");
+            dataSource.setAudioFile(AudioFile, null);
             // Loop until last utterance in the audio file has been decoded, in which case the recognizer will return null.
             Result result;
             String stringline = "";
