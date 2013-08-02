dfcb6fc08094095c87890e441e6ad03df018641a
Audrius <momantai0@gmail.com>
2013-08-02 19:01:55 +0300

    Removed unused method
    Change GetStringFromAudio2 to GetStringFromAudio.

--- SpeechRecognizerService/src/java/org/me/Sphinx/Sphinx4Service.java
+++ SpeechRecognizerService/src/java/org/me/Sphinx/Sphinx4Service.java
@@ -1,79 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.me.Sphinx;
 
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 
-
 import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
 import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
-import java.io.File;
 
+import java.io.File;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.net.URL;
 import java.io.FileOutputStream;
 
-/**
- *
- * @author Studentas
- */
 @WebService(serviceName = "Sphinx4Service")
 public class Sphinx4Service {
-
-    /**-----------------------------------------------------------------------
-     * Get string type text from Audio file (Audio file must be already saved)
-     -----------------------------------------------------------------------*/
-    @WebMethod(operationName = "GetStringFromAudio")
-    public String GetStringFromAudio() {
-        String Text;
-        File AudioFile = new File("C:\\Users\\Studentas\\Desktop\\pause.wav");
-        Sphinx4Service.Transcriber newMethod = new Sphinx4Service.Transcriber();
-        try
-        {
-            Text = newMethod.Recognise(AudioFile);
-        }
-        catch(Exception e)
-        {
-            return null;
-        }
-        return Text;
-    }
     
-    public class Transcriber {
-        
-        public String Recognise(File AudioFile) throws IOException, UnsupportedAudioFileException {
-            URL configURL = org.me.Sphinx.Sphinx4Service.class.getResource("config.xml");
-            ConfigurationManager cm = new ConfigurationManager(configURL);
-            Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
-            /* allocate the resource necessary for the recognizer */
-            recognizer.allocate();
-            // configure the audio input for the recognizer
-            AudioFileDataSource dataSource = (AudioFileDataSource) cm.lookup("audioFileDataSource");
-            dataSource.setAudioFile(AudioFile, null);
-            // Loop until last utterance in the audio file has been decoded, in which case the recognizer will return null.
-            Result result;
-            String stringline = "";
-            while ((result = recognizer.recognize())!= null) {
-                    stringline = stringline + result.getBestResultNoFiller() + ' ';
-            }
-            return stringline;
-        }
-    }
-
     /**-----------------------------------------------------------------------
      * Get string type text from Audio file (Audio file transmit through parameters)
      -----------------------------------------------------------------------*/
-    @WebMethod(operationName = "GetStringFromAudio2")
-    public String GetStringFromAudio2(@WebParam(name = "ByteStream") byte[] ByteStream) {
+    @WebMethod(operationName = "GetStringFromAudio")
+    public String GetStringFromAudio(@WebParam(name = "ByteStream") byte[] ByteStream) {
         String Text;
-        Sphinx4Service.Transcriber2 newMethod = new Sphinx4Service.Transcriber2();
+        Sphinx4Service.Transcriber newMethod = new Sphinx4Service.Transcriber();
         if (ByteStream!=null)
         {
             File AudioFile = frombytes(ByteStream);
@@ -91,7 +42,7 @@ public class Sphinx4Service {
             return "null";
     }
     
-    public class Transcriber2 {
+    public class Transcriber {
         
         public String Recognise(File AudioFile) throws IOException, UnsupportedAudioFileException {
             URL configURL = org.me.Sphinx.Sphinx4Service.class.getResource("config.xml");
