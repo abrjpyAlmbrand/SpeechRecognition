bc1b914264fe6d4e14292b0b2f236a3d0e09d95c
Mindaugas <mindaugas.dragunas@gmail.com>
2013-08-12 12:47:40 +0300

    added google speech recognition api  first attempt

--- SpeechRecognizerServiseForTomCat/src/java/Sphinx/Sphinx4Service.java
+++ SpeechRecognizerServiseForTomCat/src/java/Sphinx/Sphinx4Service.java
@@ -8,12 +8,19 @@ import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
 import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 
 import java.io.File;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.net.URL;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebService(serviceName = "Sphinx4Service")
 public class Sphinx4Service {
@@ -80,4 +87,48 @@ public class Sphinx4Service {
         }        
         return AudioFile;
     }
+
+
+/*
+ * 
+ * Google Speech api
+ */
+@WebMethod(operationName = "GetStringFromGoogle")
+    public String GetStringFromGoogle(@WebParam(name = "ByteStream") byte[] ByteStream) {
+      try {
+    String request = "https://www.google.com/"+"speech-api/v1/recognize?"+ 
+                        "xjerr=1&client=speech2text&lang=en-US&maxresults=10";
+            
+      URL url = new URL(request); 
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();           
+      connection.setDoOutput(true);
+      connection.setDoInput(true);
+      connection.setInstanceFollowRedirects(false); 
+      connection.setRequestMethod("POST"); 
+      connection.setRequestProperty("Content-Type", "audio/x-flac; rate=16000"); 
+      connection.setRequestProperty("User-Agent", "speech2text"); 
+      connection.setConnectTimeout(60000);
+      connection.setUseCaches (false);
+       DataOutputStream wr = new DataOutputStream(connection.getOutputStream ());
+       wr.write(ByteStream);
+       wr.flush();
+       wr.close();
+       connection.disconnect();
+       BufferedReader in = new BufferedReader( new InputStreamReader(connection.getInputStream()));
+       String decodedString;  
+       String aaa = "";
+       while ((decodedString = in.readLine()) != null) {
+		aaa += decodedString;
+       }
+       return aaa;
+          
+      }
+      catch (Exception ee){
+          return ee.toString();
+      }
+                      
+
+}
+
+
 }
